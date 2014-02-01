1b04c1bff75cefd56eaa60fde6223f2d548d34af
Ralph Ankele <ralph.ankele@student.tugraz.at>
2014-02-01 22:13:40 +0100

    Call hierachy for encrypt, decrypt

--- simon.c
+++ simon.c
@@ -5,3 +5,22 @@
 
 #include "simon.h"
 
+int keySchedule(){
+    return 0;
+}
+
+int encrypt(uint64_t plaintext, uint64_t ciphertext, uint64_t key){
+    return encryptRounds(plaintext, ciphertext, key, ROUNDS);
+}
+
+int encryptRounds(uint64_t plaintext, uint64_t ciphertext, uint64_t key, int rounds){
+    return 0;
+}
+
+int decrypt(uint64_t plaintext, uint64_t ciphertext, uint64_t key){
+    return decryptRounds(plaintext, ciphertext, key, ROUNDS);
+}
+
+int decryptRounds(uint64_t plaintext, uint64_t ciphertext, uint64_t key, int rounds){
+    return 0;
+}
